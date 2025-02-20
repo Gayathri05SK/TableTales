4b6deebddc334c52385a1ca44f4624563ad67547
Gayathri SK <131677111+Gayathri05SK@users.noreply.github.com>
2025-02-20 22:52:05 +0530

    Update reservationRoutes.js

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Reservation = require("../models/Reservation");
+const Reservation = require("../models/reservation");
 
 const router = express.Router();
 
